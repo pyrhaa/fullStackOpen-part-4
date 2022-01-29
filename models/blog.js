0a1b9368bf06a04514c5d04a6154bd5fd8c9dc20
pyrhaa <sboubtane@protonmail.com>
2022-01-29 15:46:25 +0100

    models ok

--- models/blog.js
+++ models/blog.js
@@ -18,4 +18,12 @@ const blogSchema = new mongoose.Schema({
   likes: Number
 });
 
-const Blog = mongoose.model('Blog', blogSchema);
+blogSchema.set('toJSON', {
+  transform: (doc, returnedObj) => {
+    returnedObj.id = returnedObj._id.toString();
+    delete returnedObj._id;
+    delete returnedObj.__v;
+  }
+});
+
+module.exports = mongoose.model('Blog', blogSchema);
