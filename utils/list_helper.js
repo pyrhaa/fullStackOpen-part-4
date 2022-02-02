6d0e446494b8b5279650d5a4f1dd26f2f81d7e99
pyrhaa <sboubtane@protonmail.com>
2022-02-02 16:03:11 +0100

    tomorrow todo lodash exercices

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,33 +1,91 @@
-const dummy = (blogs) => {
-  return 1;
-};
+const _ = require('lodash');
 
-const totalLikes = (blogs) => {
-  const reducer = (sum, blog) => sum + blog.likes;
+// const dummy = (blogs) => {
+//   return 1;
+// };
 
-  return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0);
-};
+// const totalLikes = (blogs) => {
+//   const reducer = (sum, blog) => sum + blog.likes;
 
-const favoriteBlog = (blogs) => {
-  if (blogs.length === 0) {
-    return 'No Blog, the is empty';
-  } else {
-    const mostLikedBlog = blogs.reduce((prev, current) =>
-      prev.likes > current.likes ? prev : current
-    );
+//   return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0);
+// };
 
-    const formatReturn = {
-      title: mostLikedBlog.title,
-      author: mostLikedBlog.author,
-      likes: mostLikedBlog.likes
-    };
+// const favoriteBlog = (blogs) => {
+//   if (blogs.length === 0) {
+//     return 'No Blog, the is empty';
+//   } else {
+//     const mostLikedBlog = blogs.reduce((prev, current) =>
+//       prev.likes > current.likes ? prev : current
+//     );
 
-    return formatReturn;
+//     const formatReturn = {
+//       title: mostLikedBlog.title,
+//       author: mostLikedBlog.author,
+//       likes: mostLikedBlog.likes
+//     };
+
+//     return formatReturn;
+//   }
+// };
+
+///////////////////// Using Lodash library /////////////////////
+const test = [
+  {
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: '5a422b3a1b54a676234d17f9',
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+    __v: 0
+  },
+  {
+    _id: '5a422b891b54a676234d17fa',
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    likes: 10,
+    __v: 0
+  },
+  {
+    _id: '5a422ba71b54a676234d17fb',
+    title: 'TDD harms architecture',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+    likes: 0,
+    __v: 0
+  },
+  {
+    _id: '5a422bc61b54a676234d17fc',
+    title: 'Type wars',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+    likes: 2,
+    __v: 0
   }
-};
+];
 
-module.exports = {
-  dummy,
-  totalLikes,
-  favoriteBlog
-};
+const mostBlog = (blogs) => {};
+
+console.log(mostBlog(test));
+
+// module.exports = {
+//   dummy,
+//   totalLikes,
+//   favoriteBlog
+// };
