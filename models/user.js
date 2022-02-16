55b853f1abdbd4a4853d585c8f5466285eccd452
pyrhaa <sboubtane@protonmail.com>
2022-02-16 15:54:59 +0100

    controllers post router for new user creation

--- models/user.js
+++ models/user.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  name: String,
-  passwordHash: String,
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  passwordHash: {
+    type: String,
+    required: true
+  },
   blogs: [
     {
       type: mongoose.Schema.Types.ObjectId,
