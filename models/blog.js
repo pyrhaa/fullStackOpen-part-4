fe5b3d1b2e8e12b2b657968f8a15c3965304dd2f
pyrhaa <sboubtane@protonmail.com>
2022-02-16 15:33:03 +0100

    ref are stored in both docs user/blogs

--- models/blog.js
+++ models/blog.js
@@ -17,6 +17,10 @@ const blogSchema = new mongoose.Schema({
     type: Number,
     required: true,
     default: 0
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
   }
 });
 
