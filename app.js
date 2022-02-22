80654176b121451f6dc55c1ebd3db79d6e106a71
pyrhaa <sboubtane@protonmail.com>
2022-02-22 14:50:33 +0100

    add the new router login to app

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require('cors');
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
@@ -21,8 +22,9 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
-app.use('/api/blogs', blogsRouter);
+app.use('/api/login', loginRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/blogs', blogsRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
