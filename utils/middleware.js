dc73e5c6839d2a4dd1ea224c15fffc245c462a62
pyrhaa <sboubtane@protonmail.com>
2022-02-23 12:36:33 +0100

    handling error JsonWebTokenError

--- utils/middleware.js
+++ utils/middleware.js
@@ -5,14 +5,17 @@ const unknownEndpoint = (req, res) => {
 };
 
 const errorHandler = (error, req, res, next) => {
-  logger.error(error.message);
-
   if (error.name === 'CastError' && error.kind === 'ObjectId') {
     return res.status(400).send({ error: 'malformatted id' });
   } else if (error.name === 'ValidationError') {
     return res.status(400).json({ error: error.message });
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({
+      error: 'invalid token'
+    });
   }
 
+  logger.error(error.message);
   next(error);
 };
 
